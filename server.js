832862735394251cbd2e096ae794b703fe1f837d
taron <t.mkrtchyan@inorain.com>
2024-03-09 12:18:14 -0800

    docker

--- server.js
+++ server.js
@@ -85,7 +85,6 @@ const start = async () => {
         }
       });
     });
-
     server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   } catch (error) {
     console.log(error);
